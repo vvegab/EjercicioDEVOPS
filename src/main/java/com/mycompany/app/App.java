be2ff6e3241a87f15a33d7c5bf8a81a00e351c17
Eduardo Díaz <ediaz@dcc8090.cl>
2023-07-19 19:14:12 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,8 +16,7 @@ public class App {
     if (a == b || a == c || b == c) {
       return "es un triangulo isoceles";
     }
-    // TODO: este mensaje está mal, si pasó las validaciones entonces es un triángulo escaleno
-    return "error no es un triangulo";
+    return "es un triangulo escaleno";
   }
 
   public static void main(String[] args) {
