6e5dd436a7bf38159ad9b1d63501891e7256408a
Eduardo Díaz <ediaz@dcc8090.cl>
2023-07-19 14:01:36 -0400

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -14,7 +14,7 @@ public class AppTest {
     assertEquals("error no es un triangulo", App.triangulo(0, 0, 0));
     assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
     assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
-    assertEquals("es un triangolo escaleno", App.triangulo(3, 4, 6));
-    assertEquals("es un triangolo pitagorico", App.triangulo(3, 4, 5));
+    assertEquals("es un triangulo escaleno", App.triangulo(3, 4, 6));
+    assertEquals("es un triangulo pitagorico", App.triangulo(3, 4, 5));
   }
 }
